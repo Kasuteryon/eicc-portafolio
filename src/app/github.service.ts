efeb0f2503d4e4e85bf4f4bb3a8198285c647941
Kasuteryon <eduardo.castellon66i@std.uni.edu.ni>
2022-04-18 01:36:28 -0600

    Funcionan mis repos, imagenes corregidas

--- src/app/github.service.ts
+++ src/app/github.service.ts
@@ -6,18 +6,14 @@ import {map} from 'rxjs/operators';
   providedIn: 'root'
 })
 export class GithubService {
-  private readonly url = 'https://api.github.com/users/leifermendez/repos';
+  private readonly url = 'https://api.github.com/users/kasuteryon/repos';
   private readonly urlRepoProfile = 'https://api.github.com/repos/leifermendez/portfolio-web';
 
   constructor(private httpClient: HttpClient) {
   }
 
   loadRepos = () => {
-    return this.httpClient.get(`${this.url}?type=owner&per_page=100`)
-      .pipe(
-        map((item: any) => item.filter(value => !value.fork)),
-        map((item: any) => item.filter(value => (value.stargazers_count > 3)))
-      );
+    return this.httpClient.get(`${this.url}?type=owner&per_page=100`);
   };
 
   getRepo = () => {
